87a76165c0c24368a48aee32d9819b0cf7fcb125
KevinLZC <klazaro0@ucol.mx>
2023-03-23 08:29:19 -0600

    Implementación de documentación

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require('express'); //inyección de la dependencia
 const app = express();
-let port = process.env.PORT || 3000;
-app.use('/assets', express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
+let port = process.env.PORT || 3000; //declaración del puerto de escucha
+app.use('/assets', express.static(__dirname + '/public')); //contenido estático
+app.set('view engine', 'ejs'); //declaración de motor de renderizado
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //ruta raiz
     res.send(`<html><head><link href=assets/style.css type=text/css rel=stylesheet /><title>Práctica 14</title></head><body><h1>Hello World!</h1></body></html>`);
 });
-app.get('/person/:id', (req, res) => {
+app.get('/person/:id', (req, res) => { //ruta para prueba de envío de datos por url
     res.render('person', {ID: req.params.id, Qstr: req.query.qstr});
 });
-app.get('/persons/:id', (req, res) => {
+app.get('/persons/:id', (req, res) => { //ruta de ejercicio para repetición y selección de mensaje
     res.render('persons', {ID: req.params.id, message: req.query.message, times: req.query.times});
 });
 
